69bafb2993154c5e21a6ff50170c5be11dbff71f
nornjuidat <njuidatnor750@gmail.com>
2026-04-14 20:31:17 +0300

    working

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(express.json());
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+let db_M = require('./database');
+global.db_pool = db_M.pool;
+
 const path = require('path');
  app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port (http://localhost:${port}`);
